b3091c65194961ec68cc00d909fba9175097779d
kazuhiko1979 <challengevolley1979@gmail.com>
2020-06-17 19:03:34 +0900

    Update

--- drone_manager.py
+++ drone_manager.py
@@ -8,15 +8,19 @@ import time
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
 
+DEFAULT_DISTANCE = 0.30
 
 class DroneManager(object):
     def __init__(self, host_ip='192.168.10.2', host_port=8889,
-                 drone_ip='192.168.10.1', drone_port=8889):
+                 drone_ip='192.168.10.1', drone_port=8889,
+                 is_imperial=False):
+        # self.receive_response_thread = None
         self.host_ip = host_ip
         self.host_port = host_port
         self.drone_ip = drone_ip
         self.drone_port = drone_port
         self.drone_address = (drone_ip, drone_port)
+        self.is_imperial = is_imperial
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.host_ip, self.host_port))
 
@@ -25,7 +29,7 @@ class DroneManager(object):
         self._response_thread = threading.Thread(target=self.receive_response,
                                            args=(self.stop_event, ))
 
-        self.receive_response_thread.start()
+        self._response_thread.start()
         self.send_command('command')
         self.send_command('streamon')
 
@@ -77,36 +81,50 @@ class DroneManager(object):
     def land(self):
         self.send_command('land')
 
+    def move(self, direction, distance):
+        distance = float(distance)
+        if self.is_imperial:
+            distance = int(round(distance * 30.48))
+        else:
+            distance = int(round(distance * 100))
+        # return self.send_command(f'{distance} {distance}') < Python 3.6
+        return self.send_command('{0} {1}'.format(distance, distance))
+
+    def up(self, distance=DEFAULT_DISTANCE):
+        return self.move('up', distance)
+
+    def down(self, distance=DEFAULT_DISTANCE):
+        return self.move('down', distance)
+
+    def left(self, distance=DEFAULT_DISTANCE):
+        return self.move('left', distance)
+
+    def right(self, distance=DEFAULT_DISTANCE):
+        return self.move('right', distance)
+
+    def forward(self, distance=DEFAULT_DISTANCE):
+        return self.move('forward', distance)
+
+    def back(self, distance=DEFAULT_DISTANCE):
+        return self.move('back', distance)
+
 
 if __name__ == '__main__':
     drone_manager = DroneManager()
     drone_manager.takeoff()
 
     time.sleep(10)
+    drone_manager.forward()
+    time.sleep(5)
+    drone_manager.right()
+    time.sleep(5)
+    drone_manager.back()
+    time.sleep(5)
+    drone_manager.left()
+    time.sleep(5)
+    drone_manager.up()
+    time.sleep(5)
+    drone_manager.down()
 
     drone_manager.land()
     drone_manager.stop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
