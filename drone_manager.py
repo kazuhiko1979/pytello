1c305f699ea71916c380447df718d64a28b69b48
kazuhiko1979 <challengevolley1979@gmail.com>
2020-06-17 18:14:42 +0900

    First update

---
+++ drone_manager.py
@@ -0,0 +1,112 @@
+import logging
+import socket
+import sys
+import threading
+import time
+
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+
+class DroneManager(object):
+    def __init__(self, host_ip='192.168.10.2', host_port=8889,
+                 drone_ip='192.168.10.1', drone_port=8889):
+        self.host_ip = host_ip
+        self.host_port = host_port
+        self.drone_ip = drone_ip
+        self.drone_port = drone_port
+        self.drone_address = (drone_ip, drone_port)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.host_ip, self.host_port))
+
+        self.response = None
+        self.stop_event = threading.Event()
+        self._response_thread = threading.Thread(target=self.receive_response,
+                                           args=(self.stop_event, ))
+
+        self.receive_response_thread.start()
+        self.send_command('command')
+        self.send_command('streamon')
+
+    def receive_response(self, stop_event):
+        while not stop_event.is_set():
+            try:
+                self.response, ip = self.socket.recvfrom(3000)
+                logger.info({'action': 'receive_response',
+                             'response': self.response})
+            except socket.error as ex:
+                logger.error({'action': 'receive_response',
+                              'ex': ex})
+                break
+
+    def __dell__(self):
+        self.stop()
+
+    def stop(self):
+        self.stop_event.set()
+        retry = 0
+        while self._response_thread.is_alive():
+            time.sleep(0.3)
+            if retry > 30:
+                break
+            retry += 1
+        self.socket.close()
+
+    def send_command(self, command):
+        logging.info({'action': 'send_command', 'command': command})
+        self.socket.sendto(command.encode('utf-8'), self.drone_address)
+
+        retry = 0
+        while self.response is None:
+            time.sleep(0.3)
+            if retry > 3:
+                break
+            retry += 1
+
+        if self.response is None:
+            response = None
+        else:
+            response = self.response.decode('utf-8')
+        self.response = None
+        return response
+
+    def takeoff(self):
+        self.send_command('takeoff')
+
+    def land(self):
+        self.send_command('land')
+
+
+if __name__ == '__main__':
+    drone_manager = DroneManager()
+    drone_manager.takeoff()
+
+    time.sleep(10)
+
+    drone_manager.land()
+    drone_manager.stop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
